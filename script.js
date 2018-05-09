66b56836e287d059c09175f47129a61d57f975f8
Dimkajasons <dimkajasons@gmail.com>
2018-05-10 01:18:54 +0300

    almost done

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 //here logic goes
 
 //Вывести в браузер результаты первый четырех заданий из arrays.js
+var actorsArr = document.querySelectorAll('.list > li');
+var result = document.querySelector('#result');
+console.log(actorsArr)
+
+function getActorsBeforeYear(year) {
+    for (let i = 0; i < actorsArr.length; i++){
+        if (actorsArr[i].dataset.year < year) {
+            result.appendChild(actorsArr[i].cloneNode(true));
+        }
+    }
+}
+
+function getFullName() {
+    let newArr = [];
+    for (let i = 0; i < actorsArr.length; i++) {
+        newArr.push(actorsArr[i].firstChild.innerText);
+    }
+    result.innerHTML = newArr;
+}
+
+// function sortByAge() {
+//     for (let i = 0; i < actorsArr.length; i++){
+
+//     }
+// }
+
+function getTotalAge(){
+    let num = 0;
+    for (let i = 0; i < actorsArr.length; i++) {
+        num += 2018-actorsArr[i].lastChild.innerText;
+    }
+    result.innerHTML = num;
+}
