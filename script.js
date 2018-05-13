142465e63b8e5591888ce662ac884a2b0557feb8
Dimkajasons <dimkajasons@gmail.com>
2018-05-13 22:58:45 +0300

    edit for new hw

--- script.js
+++ script.js
@@ -1,47 +1,30 @@
 //here logic goes
 
 //Вывести в браузер результаты первый четырех заданий из arrays.js
+
+var actors = [
+    { firstName: "Tom", lastName: "Cruise", year: 1962 },
+    { firstName: "Jackie", lastName: "Chan", year: 1954 },
+    { firstName: "Brad", lastName: "Pitt", year: 1963 },
+    { firstName: "Jean-Claude", lastName: "Van Damme", year: 1960 },
+    { firstName: "Bruce", lastName: "Willis", year: 1955 },
+    { firstName: "Arnold", lastName: "Schwarzenegger", year: 1946 },
+    { firstName: "Silvester", lastName: "Stallone", year: 1947 },
+]
 var actorsArr = document.querySelectorAll('.list > li');
 var result = document.querySelector('#result');
-console.log(actorsArr)
+var button = document.querySelector('#button')
+// button.addEventListener('click', asyncFunc())
 
-function getActorsBeforeYear(year) {
+function asyncFunc (order, arr) {
     result.innerHTML = '';
-    for (let i = 0; i < actorsArr.length; i++){
-        if (actorsArr[i].dataset.year < year) {
-            result.appendChild(actorsArr[i].cloneNode(true));
-        }
-    }
-}
-
-function getFullName() {
-    let newArr = [];
-    for (let i = 0; i < actorsArr.length; i++) {
-        newArr.push(actorsArr[i].firstChild.innerText);
-    }
-    result.innerHTML = newArr;
-}
-
-function sortByAge() {
-    let resultArr = document.querySelectorAll('#result > li');
-    result.appendChild(actorsArr[0].cloneNode(true))
-    for (let i = 1; i < actorsArr.length; i++) {
-        resultArr = document.querySelectorAll('#result > li');
-        for (let j = 0; j < resultArr.length; j++) {
-            if (resultArr[j].lastChild.innerHTML < actorsArr[i].lastChild.innerHTML) {
-                result.insertBefore(actorsArr[i], resultArr[j]);
-                break;
-            } else {
-                result.appendChild(actorsArr[i]);
-            }
-        }
-    }
-}
-
-function getTotalAge(){
-    let num = 0;
-    for (let i = 0; i < actorsArr.length; i++) {
-        num += 2018-actorsArr[i].lastChild.innerText;
-    }
-    result.innerHTML = num;
+    let localActors = arr.slice();
+    if (!order) {localActors.reverse()}
+    localActors.map(function(actor, index){
+        let actorHtmlElement = document.createElement('div');
+        actorHtmlElement.textContent = `${actor.firstName} ${actor.lastName}`
+            setTimeout(function(){  
+                    result.appendChild(actorHtmlElement); 
+            }, index * 100 );
+    })
 }
