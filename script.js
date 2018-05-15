4b51ac79b565e3401e1fc5334a3cbf5d5bdc83ac
Dimkajasons <dimkajasons@gmail.com>
2018-05-16 00:15:32 +0300

    edited for events hw

--- script.js
+++ script.js
@@ -11,38 +11,85 @@ var actors = [
     { firstName: "Arnold", lastName: "Schwarzenegger", year: 1946 },
     { firstName: "Silvester", lastName: "Stallone", year: 1947 },
 ]
-var actorsArr = document.querySelectorAll('.list > li');
-var result = document.querySelector('#result');
-var button = document.querySelector('#button');
-var button2 = document.querySelector('#button2');
 
-asyncFuncCallerTrue = function() {
-    asyncFunc(true, actors)
-}
-asyncFuncCallerFalse = function () {
-    asyncFunc(false, actors)
-}
-button.addEventListener('click', asyncFuncCallerTrue);
-button2.addEventListener('click', asyncFuncCallerFalse);
+var actorsList = document.querySelectorAll('#actor');
+var mainDiv = document.querySelector('#actors-list');
+var button = document.querySelector('#button')
 
-function asyncFunc (order, arr) {
-    result.innerHTML = '';
-    let localActors = arr.slice();
-    if (!order) {
-        localActors.forEach(function (actor, index) {
-            let actorHtmlElement = document.createElement('div');
-            actorHtmlElement.textContent = `${actor.firstName} ${actor.lastName}`
-            setTimeout(function () {
-                result.appendChild(actorHtmlElement);
-            }, (arr.length - index) * 100);
-        })
-    } else {
-        localActors.forEach(function(actor, index){
-            let actorHtmlElement = document.createElement('div');
-            actorHtmlElement.textContent = `${actor.firstName} ${actor.lastName}`
-                setTimeout(function(){  
-                        result.appendChild(actorHtmlElement); 
-                }, index * 100 );
-        })
-    }
+function hideElem() {
+    actorsList.forEach(function (el, i) {
+        if (i !== 0) { el.classList.add('hidden') }
+    })
 }
+hideElem();
+
+button.addEventListener('click', function() {
+    actorsList.forEach((el) => el.classList.remove('hidden'))
+})
+
+mainDiv.addEventListener('click', function(e) {
+    actorsList.forEach((el) => el.classList.add('hidden'))
+    e.target.classList.remove('hidden')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var actorsArr = document.querySelectorAll('.list > li');
+// var result = document.querySelector('#result');
+// var button = document.querySelector('#button');
+// var button2 = document.querySelector('#button2');
+
+// asyncFuncCallerTrue = function() {
+//     asyncFunc(true, actors)
+// }
+// asyncFuncCallerFalse = function () {
+//     asyncFunc(false, actors)
+// }
+// button.addEventListener('click', asyncFuncCallerTrue);
+// button2.addEventListener('click', asyncFuncCallerFalse);
+
+// function asyncFunc (order, arr) {
+//     result.innerHTML = '';
+//     let localActors = arr.slice();
+//     if (!order) {
+//         localActors.forEach(function (actor, index) {
+//             let actorHtmlElement = document.createElement('div');
+//             actorHtmlElement.textContent = `${actor.firstName} ${actor.lastName}`
+//             setTimeout(function () {
+//                 result.appendChild(actorHtmlElement);
+//             }, (arr.length - index) * 100);
+//         })
+//     } else {
+//         localActors.forEach(function(actor, index){
+//             let actorHtmlElement = document.createElement('div');
+//             actorHtmlElement.textContent = `${actor.firstName} ${actor.lastName}`
+//                 setTimeout(function(){  
+//                         result.appendChild(actorHtmlElement); 
+//                 }, index * 100 );
+//         })
+//     }
+// }
